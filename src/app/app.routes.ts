df2b357a7c309234e683ac8addca2483d0b6dbe5
Wren <grahamgrasley@gmail.com>
2024-09-12 16:55:30 -0400

    Add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DonutsComponent } from './components/donuts/donuts.component';
 import { DonutDetailsComponent } from './components/donut-details/donut-details.component';
 import { CartComponent } from './components/cart/cart.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
 	{path: "", redirectTo: "/Donuts", pathMatch: "full"},
 	{path:"Donuts", component: DonutsComponent},
 	{path:"Donuts/:id", component: DonutDetailsComponent},
-	{path:"Checkout", component: CartComponent}
+	{path:"Checkout", component: CartComponent},
+	{path:"**", component: NotFoundComponent}
 ];
